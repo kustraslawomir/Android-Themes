a695e9a0f9190a2adce5d1e1355ea99792d58cf7
slawomir.kustra <slawomir.kustra@witchcraftstudios.com>
2018-11-06 11:19:56 +0100

    change application theme with recreate method

--- app/src/main/java/themes/kustra/pl/themes/MainActivity.kt
+++ app/src/main/java/themes/kustra/pl/themes/MainActivity.kt
@@ -1,24 +1,30 @@
 package themes.kustra.pl.themes
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import themes.kustra.pl.themes.utils.Constants.Companion.BLUE
+import themes.kustra.pl.themes.utils.Constants.Companion.THEME_COLOR
+import themes.kustra.pl.themes.utils.Constants.Companion.GREEN
+import themes.kustra.pl.themes.utils.Constants.Companion.RED
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                saveTheme(RED)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                saveTheme(GREEN)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                saveTheme(BLUE)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -27,8 +33,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(getSavedTheme())
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun saveTheme(color: String) {
+        val editor = getPreferences(Activity.MODE_PRIVATE).edit()
+        editor.putString(THEME_COLOR, color)
+        editor.apply()
+        recreate()
+    }
+
+    private fun getSavedTheme(): Int {
+        val theme = getPreferences(Context.MODE_PRIVATE).getString(THEME_COLOR, RED)
+        return when (theme) {
+            RED -> R.style.RedTheme
+            BLUE -> R.style.BlueTheme
+            GREEN -> R.style.GreenTheme
+            else -> R.style.RedTheme
+        }
     }
 }
---
+++ app/src/main/java/themes/kustra/pl/themes/utils/Constants.kt
@@ -0,0 +1,10 @@
+package themes.kustra.pl.themes.utils
+
+class Constants {
+    companion object {
+        const val THEME_COLOR = "color"
+        const val RED = "red"
+        const val BLUE = "blue"
+        const val GREEN = "green"
+    }
+}
